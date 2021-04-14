a8e5a8eec59932143af7da50b5331d7830c21fe2
Ivan Malkov <ozonep@yandex.ru>
2021-04-14 12:13:28 +0300

    refactor: 💡 updated WebPack config + delted few files

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
-const path = require('path')
+const path = require('path');
+const TerserPlugin = require("terser-webpack-plugin");
+
 
 module.exports = {
   target: "webworker",
+  mode: "production",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "lightning-fs.min.js",
     library: "LightningFS",
-    globalObject: 'self',
-    libraryTarget: "umd",
+    libraryTarget: "global",
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({ parallel: true })]
   },
 };
